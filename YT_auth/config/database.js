873ef5e0670e1905846e3d31b3b1057c1e877bd9
Nishant <nishantkarekar98@gmail.com>
2026-05-06 10:07:23 +0530

    "add new changes"

--- YT_auth/config/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import config from "./config.js";
-
-const connectDB = async() =>{
-    await mongoose.connect(config.MONGODB_URL)
-    console.log("connected to db")
-}
-
-export default connectDB;
