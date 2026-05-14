92ec65d29f1447cdd7e32466bddc125c65a14887
Nishant <nishantkarekar98@gmail.com>
2026-05-14 15:48:04 +0530

    add changes

--- Auth_sys/backend/server.js
+++
@@ -1,9 +0,0 @@
-import app from "./src/app.js";
-import config from "./src/config/config.js";
-import dbConnection from "./src/config/dbConnection.js";
-
-dbConnection();
-
-app.listen(config.PORT, () => {
-  console.log(`server runnnig on ${config.PORT}`);
-});
