0c89846b267aada18b74ad9ae013861099d98b5b
Nishant <nishantkarekar98@gmail.com>
2026-05-27 10:21:20 +0530

    "Add subscription mode"

---
+++ chai_backend/src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamp: true }
+);
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
