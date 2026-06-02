7831483641a312b4cb8b095125830b7d8ba64aee
Nishant <nishantkarekar98@gmail.com>
2026-06-02 13:30:50 +0530

    "add get watch history"

--- chai_backend/src/controllers/user.controller.js
+++ chai_backend/src/controllers/user.controller.js
@@ -443,4 +443,10 @@ export const getWatchHistory = asyncHandler(async (req, res) => {
       },
     },
   ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, user[0].watchHistory, "watch history successfully!!")
+    );
 });
