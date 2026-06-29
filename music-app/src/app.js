142ab7ce0c0d6bdcfeb49747c9c7dc3550b0f19f
Nishant <nishantkarekar98@gmail.com>
2026-06-29 13:15:29 +0530

    chagne in folders

--- music-app/src/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import userRouter from "./routes/auth.route.js";
-
-const app = express();
-app.use(express.json({ limit: "10mb" }));
-
-app.use("/api/v1", userRouter);
-
-export default app;
--- music-app/src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { login, registerUser } from "../controllers/auth.controller.js";
-
-const router = Router();
-
-router.route("/register").post(registerUser);
-router.route("/login").post(login);
-
-export default router;
