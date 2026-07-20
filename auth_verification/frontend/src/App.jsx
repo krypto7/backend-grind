9ec04db22587b57c88b5d37a1efab514f7c645ac
Nishant <nishantkarekar98@gmail.com>
2026-07-20 15:38:36 +0530

    "add frontend UI"

---
+++ auth_verification/frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ auth_verification/frontend/src/pages/Signup.jsx
@@ -0,0 +1,140 @@
+import { Button } from "@/components/ui/button";
+import { Eye, Loader2 } from "lucide-react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+// import { useState } from "react";
+
+function Signup() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+
+  const handlePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name);
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div className="relative w-full h-screen bg-green-100 overflow-hidden">
+      <div className="min-h-screen flex flex-col to-muted/20">
+        <div className="flex-1 flex items-center justify-center p-4">
+          <div className="w-full max-w-md space-y-6">
+            <div className="text-center space-y-2 flex flex-col items-center">
+              <h1 className="text-3xl font-bold tracking-tight text-green-600">
+                Create your account
+              </h1>
+              <p>Start organizing your throughts and ideas todays</p>
+
+              <Card className="w-full max-w-sm">
+                <CardHeader>
+                  <CardTitle>Sign up</CardTitle>
+                  <CardDescription>
+                    Enter your email below to login to your account
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex flex-col gap-6">
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">Username</Label>
+                      <Input
+                        id="username"
+                        name="username"
+                        type={"text"}
+                        placeholder="joe doe"
+                        value={formData.username}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">Email</Label>
+                      <Input
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <div className="flex items-center">
+                        <Label htmlFor="password">Password</Label>
+                      </div>
+                      <div className="relative">
+                        <Input
+                          id="password"
+                          name="password"
+                          type={showPassword ? "text" : "password"}
+                          value={formData.password}
+                          onChange={handleChange}
+                          placeholder="Enter your password"
+                          required
+                        />
+                        <button
+                          type="button"
+                          variant="ghost"
+                          size="icon"
+                          onClick={handlePassword}
+                          className="absolute right-0 top-0 h-full px-3"
+                        >
+                          <Eye />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </CardContent>
+                <CardFooter className="flex-col gap-2">
+                  <Button
+                    type="button"
+                    className="w-full"
+                    onClick={handleSubmit}
+                  >
+                    {isLoading ? (
+                      <>
+                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                        creating account
+                      </>
+                    ) : (
+                      "Signup"
+                    )}
+                  </Button>
+                </CardFooter>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
